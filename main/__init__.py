45fd8df6dd5d8a4b9cc8c3dd4095b64daa5d2d5a
SARANYA BALAKRISHNAN <55033431+saranyab9064@users.noreply.github.com>
2020-06-22 18:46:16 -0700

    updated

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,8 @@
+#!/usr/local/bin/python3
 """
 A simple app to create a JWT token.
 """
+
 import os
 import logging
 import datetime
@@ -59,6 +61,11 @@ def require_jwt(function):
 @APP.route('/', methods=['POST', 'GET'])
 def health():
     return jsonify("Healthy")
+
+
+@APP.route('/env', methods=['POST', 'GET'])
+def env_check():
+    return jsonify(JWT_SECRET)
 
 
 @APP.route('/auth', methods=['POST'])
